c7469f953a89175dca2b9f2f42879ea62fe3df4c
companyadr62 <companyadr62@gmail.com>
2026-01-13 15:52:00 +1000

    Initial commit: no Pagination, only batch

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,17 +1,26 @@
 import multiprocessing
 
 # Количество воркеров
-workers = 2  # Меньше воркеров для экономии памяти
+workers = multiprocessing.cpu_count() * 2 + 1
 
-# Таймауты
-timeout = 60  # 30 секунд (увеличено для медленных запросов)
-keepalive = 2
+# Увеличенные таймауты для медленных запросов OCS
+timeout = 300  # 5 минут
+keepalive = 5
+
+# Пул воркеров
+worker_class = 'sync'  # или 'gevent' для асинхронности
+worker_connections = 1000
+
+# Перезапуск воркеров при утечке памяти
+max_requests = 1000
+max_requests_jitter = 50
 
 # Логирование
 accesslog = '-'
 errorlog = '-'
 loglevel = 'info'
 
-# Перезапуск при ошибках
-max_requests = 1000
-max_requests_jitter = 50
+# Увеличенный размер буфера для больших ответов
+limit_request_line = 8190
+limit_request_fields = 100
+limit_request_field_size = 8190
